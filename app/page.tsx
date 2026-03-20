85b00c93cf41e2c39da5cc0d5b3e52ad8c0b0e63
Praveen1116 <kpraveen1116@gmail.com>
2026-03-20 19:38:03 +0530

    Small changes in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link  from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Can you see me
+      <h1 className="mb-4 text-5xl font-mono">Learning Next</h1>
+      <Link className="border-1 p-3 rounded-xl mb-2 cursor-pointer bg-teal-300 text-black font-semibold" href="/signup">Let's Signup</Link>
+      <Link className="border-1 p-3 rounded-xl mb-2 cursor-pointer bg-green-400 text-black font-semibold" href="/signin">Let's Signin</Link>
     </div>
   );
 }
